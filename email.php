0ba357e08f4731b84272363f2a898ea0b2676bc7
Maik Freitas <maikemir2003@gmail.com>
2021-01-15 20:47:50 -0300

    Corrigindo erros

--- email.php
+++ email.php
@@ -1,27 +1,28 @@
 <?php
 if(isset($_POST['email']) && !empty($_POST['email'])){
-    $nome = addcslashes($_POST['nome']);
-    $email = addcslashes($_POST['email']);
-    $mensagem = addcslashes($_POST['mensagem']);
+$nome = addcslashes($_POST['nome']);
+$email = addcslashes($_POST['email']);
+$mensagem = addcslashes($_POST['mensagem']);
 
-    $to = "maikdev01@gmail.com";
-    $subject = "Contato - Maik";
-    $body = "Nome:".$nome. "\r\n".
-            "Email:".$email. "\r\n".
-            "Mensagem:".$mensagem;
-    $header = "From:maik.santos@ntectreinamentos.com.br"."\r\n".
-            "Reply-to:".$email."\r\n".
-            "X=Mailer:PHP/".phpversion();
-    if(mail($to,$subject,$body,$header)){
-        echo("<script>
-        window.alert('Email enviado com sucesso');
-        window.location.replace('index.php');
-        </script>");
+
+$to = "maik.dev01@gmail.com";
+$subject = "Contato - Maik";
+$body = "Nome:".$nome."\r\n".
+        "Email:".$email."\r\n".
+        "Mensagem:".$mensagem;
+$header = "From:maik@ntectreinamentos.com.br"."\r\n".
+        "Reply-to:".$email."\r\n".
+        "X=Mailer:PHP/".phpversion();
+if(mail($to,$subject,$body,$header)){
+    echo("<script>
+    window.alert('Email enviado com sucesso');
+    window.location.replace('index.php');
+    </script>");
     }else{
-        echo("<script>
-        window.alert('Não foi possivel enviar o email');
-        window.location.replace('index.php');
-        </script>");
+    echo("<script>
+    window.alert('Não foi possivel enviar o email');
+    window.location.replace('index.php');
+    </script>");
     }
 }
 
